b54b5f40f9705ae4adf241c1287558842dac3d2f
Damian <damianfilipek81@gmail.com>
2021-05-17 16:53:13 +0200

    Install nodemon, change static html sites to Handlebars

--- server.js
+++ server.js
@@ -1,36 +1,41 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
-app.use('/user/', (req, res, next) => {
-  res.show('forbidden.html');
-});
-
 app.get('/', (req, res) => {
-  res.show('home.html');
-});
-
-app.get('/home', (req, res) => {
-  res.show('home.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res, next) => {
+  res.render('history');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
-});
+  res.status(404).send('404 not found...');
+})
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
